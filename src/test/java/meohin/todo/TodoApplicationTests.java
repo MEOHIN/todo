423a1e18d05c163dcc7c225599be1f892461bb28
meohin <meohin.meo@gmail.com>
2022-04-06 19:10:28 +0900

    Update application

---
+++ src/test/java/meohin/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package meohin.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
